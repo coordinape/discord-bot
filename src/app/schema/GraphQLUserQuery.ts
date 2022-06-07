e077a6cd35bdb4218ad497edd4d7ec75305aaeb5
nonsensecodes <joshua@nonsensecodes.io>
2022-06-06 22:27:14 -0700

    updated GraphQL query and GuildMemberUpdate

--- src/app/schema/GraphQLUserQuery.ts
+++ src/app/schema/GraphQLUserQuery.ts
@@ -1,42 +1,48 @@
 import fetch from 'node-fetch';
+import Log from '../utils/Log';
 
-function fetchGraphQL(operationsDoc: string, operationName: string, variables: Record<string, any>) {
-	return fetch('undefined', {
-		method: 'POST',
-		body: JSON.stringify({
-			query: operationsDoc,
-			variables,
-			operationName,
-		}),
-	}).then(result => result.json());
-}
-  
-const operation = `
-    query MyCircles {
-      circles {
-        name
-        users {
-          id
-          name
-          profile {
-            discord_username
+const graphQLQueryObj = {
+    fetchGraphQL: async function (operationsDoc: string, operationName: string, variables: Record<string, any>) {
+        const result = await fetch('undefined', {
+            method: 'POST',
+            body: JSON.stringify({
+                query: operationsDoc,
+                variables,
+                operationName,
+            }),
+        });
+        return await result.json();
+    },
+      
+    operation: `
+        query MyCircles {
+          circles {
+            name
+            users {
+              id
+              name
+              profile {
+                discord_username
+              }
+            }
           }
         }
-      }
+      `;
+      
+    fetchMyCircles: function () {
+        return this.fetchGraphQL(this.operation, 'MyCircles', {});
     }
-  `;
-  
-function fetchMyCircles() {
-	return fetchGraphQL(operation, MyCircles, {});
 }
-  
-fetchMyCircles()
-	.then(({ data, errors }) => {
-		if (errors) {
-			console.error(errors);
-		}
-		console.log(data);
-	})
-	.catch(error => {
-		console.error(error);
-	});
+
+const graphQLCirclesQuery = graphQLQueryObj.fetchMyCircles()
+  .then(({ data, errors }) => {
+    if (errors) {
+      Log.error(errors);
+    }
+    Log.log(data);
+  })
+  .catch((error: any) => {
+    Log.error(error);
+  });
+
+  export { graphQLQueryObj, graphQLCirclesQuery };
