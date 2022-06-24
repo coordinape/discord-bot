983227d9ba8d0b472d0b0795713c0e56a5ccfe8f
nonsensecodes <joshua@nonsensecodes.io>
2022-06-23 17:59:30 -0700

    update logdna reference

--- src/app/service/constants/apiKeys.ts
+++ src/app/service/constants/apiKeys.ts
@@ -1,7 +1,7 @@
 const apiKeys = Object.freeze({
 	DISCORD_BOT_ID: process.env.DISCORD_BOT_APPLICATION_ID,
 	
-	logDNAToken: process.env.LOGDNA_TOKEN as string,
+	logDNAToken: process.env.LOGDNA_TOKEN,
 	logDNAAppName: process.env.LOGDNA_APP_NAME,
 	logDNADefault: process.env.LOGDNA_DEFAULT_LEVEL,
 	
