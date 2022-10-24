8939249d83f2ac8dcc8302d06fb522af9b6fea72
Karelian Pie <devkarelianpie@gmail.com>
2022-10-24 13:59:54 +0300

    refactor: Code cleanup

---
+++ src/app/service/ServiceSupport.ts
@@ -0,0 +1,50 @@
+import { ButtonStyle,
+	CommandContext,
+	ComponentType,
+	ComponentActionRow,
+} from 'slash-create';
+import Log from '../utils/Log';
+
+export class ServiceSupport {
+	private _ctx: CommandContext;
+
+	constructor(ctx: CommandContext) {
+		this._ctx = ctx;
+	}
+
+	async ephemeralError({ msg }: {msg?: string}): Promise<void> {
+		const row: ComponentActionRow = {
+			type: ComponentType.ACTION_ROW,
+			components: [{ type: ComponentType.BUTTON, style: ButtonStyle.LINK, label: 'Support', url: 'https://discord.coordinape.com/' }],
+		};
+		try {
+			await this._ctx.send({
+				content: msg ?? 'Something is not working. Please reach out to us and a support member will happily assist!',
+				ephemeral: true,
+				components: [row],
+			});
+		} catch (e) {
+			Log.error(e);
+		}
+	}
+
+	async ephemeralWebsite({ label, url, content }: {label: string, url: string; content: string}): Promise<void> {
+		const row: ComponentActionRow = {
+			type: ComponentType.ACTION_ROW,
+			components: [{ type: ComponentType.BUTTON, style: ButtonStyle.LINK, label, url }],
+		};
+		try {
+			await this._ctx.send({ content, ephemeral: true, components: [row] });
+		} catch (e) {
+			Log.error(e);
+		}
+	}
+
+	async ephemeralText({ text }: {text: string}): Promise<void> {
+		try {
+			await this._ctx.send({ ephemeral: true, content: text });
+		} catch (e) {
+			Log.error(e);
+		}
+	}
+}
