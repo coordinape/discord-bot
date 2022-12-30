cd4c62d8a657f2a66e8638013ee46b185e78c1e8
Karelian Pie <devkarelianpie@gmail.com>
2022-12-30 13:21:09 +0200

    feat: Add role to the message

--- src/app/server/handlers/epochNomination.ts
+++ src/app/server/handlers/epochNomination.ts
@@ -1,12 +1,28 @@
-import { ButtonBuilder, ActionRowBuilder, ButtonStyle } from 'discord.js';
+import { ButtonBuilder, ActionRowBuilder, ButtonStyle, TextChannel } from 'discord.js';
 import client from '../../app';
 import { COORDINAPE_BUTTON } from '../components';
 import { isTextChannel } from '../utils';
 import { Request, Response } from 'express';
 import { z } from 'zod';
 
+/**
+ * curl --request POST \
+  --url http://localhost:4000/api/epoch/nomination \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"channelId": "1057926498524332083",
+	"roleId": "1058334400540061747",
+	"nominee": "John Doe",
+	"nominator": "Jane Doe",
+	"nominationReason": "great contributions",
+	"numberOfVouches": 2
+}
+'
+ */
+
 const EpochNomination = z.object({
 	channelId: z.string(),
+	roleId: z.string(),
 	nominee: z.string(),
 	nominator: z.string(),
 	nominationReason: z.string(),
@@ -21,23 +37,28 @@ export default async function handler(req: Request, res: Response) {
 		
 		const channel = await client.channels.fetch(channelId);
 		if (!channel || !isTextChannel(channel)) {
-			res.status(404).send('Channel not found!');
-			return;
+			throw new Error('Channel not found!');
 		}
-
+		
 		const actions: ButtonBuilder[] = ['Vouch', 'Link'].map((label) => (new ButtonBuilder()
 			.setCustomId(`${label.toUpperCase()}_BUTTON`)
 			.setLabel(label)
 			.setStyle(ButtonStyle.Primary)));
 		const row = new ActionRowBuilder<ButtonBuilder>().addComponents([...actions, COORDINAPE_BUTTON]);
-		const message = await channel.send({ content: getMessage(data), components: [row] });
 		
+		const message = await channel.send({ content: await getContent({ channel, ...data }), components: [row] });
+
 		res.status(200).send({ createdAt: message.createdTimestamp, body: req.body });
 	} catch (error) {
 		res.status(400).send(error);
 	}
 }
 
-function getMessage({ nominee, nominator, nominationReason, numberOfVouches }: TEpochNomination) {
-	return `[role] ${nominee} was nominated by ${nominator} for ${nominationReason}!\n\nThey need ${numberOfVouches} vouches to join the circle.`;
+async function getContent({ channel, roleId, nominee, nominator, nominationReason, numberOfVouches }: { channel: TextChannel } & TEpochNomination) {
+	const guild = await client.guilds.fetch(channel.guildId);
+	const role = await guild.roles.fetch(roleId);
+	if (!role) {
+		throw new Error(`Role with id '${roleId}' not found!`);
+	}
+	return `${role} ${nominee} was nominated by ${nominator} for ${nominationReason}!\nThey need ${numberOfVouches} vouches to join the circle.`;
 }
