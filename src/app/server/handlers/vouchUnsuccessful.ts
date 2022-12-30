9381d66c58dc75cfbf814fab15045406bf6b6739
Karelian Pie <devkarelianpie@gmail.com>
2022-12-30 14:31:10 +0200

    feat: Add vouch unsuccessful handler

---
+++ src/app/server/handlers/vouchUnsuccessful.ts
@@ -0,0 +1,47 @@
+import { ButtonBuilder, ActionRowBuilder, ButtonStyle, Role } from 'discord.js';
+import client from '../../app';
+import { COORDINAPE_BUTTON } from '../components';
+import { isTextChannel } from '../utils';
+import { Request, Response } from 'express';
+import { z } from 'zod';
+
+const VouchUnsuccessful = z.object({
+	channelId: z.string(),
+	roleId: z.string(),
+	nominee: z.string(),
+});
+
+type TVouchUnsuccessful = Omit<z.infer<typeof VouchUnsuccessful>, 'channelId'>;
+
+export default async function handler(req: Request, res: Response) {
+	try {
+		const { channelId, ...data } = VouchUnsuccessful.parse(req.body);
+		
+		const channel = await client.channels.fetch(channelId);
+		if (!channel || !isTextChannel(channel)) {
+			throw new Error('Channel not found!');
+		}
+		
+		const actions: ButtonBuilder[] = ['Nominate', 'Link'].map((label) => (new ButtonBuilder()
+			.setCustomId(`${label.toUpperCase()}_BUTTON`)
+			.setLabel(label)
+			.setStyle(ButtonStyle.Primary)));
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents([...actions, COORDINAPE_BUTTON]);
+		
+		const guild = await client.guilds.fetch(channel.guildId);
+		const role = await guild.roles.fetch(data.roleId);
+		if (!role) {
+			throw new Error(`Role with id '${data.roleId}' not found!`);
+		}
+
+		const message = await channel.send({ content: await getContent({ role, ...data }), components: [row] });
+
+		res.status(200).send({ createdAt: message.createdTimestamp, body: req.body });
+	} catch (error) {
+		res.status(400).send(error);
+	}
+}
+
+async function getContent({ role, nominee }: { role: Role } & TVouchUnsuccessful) {
+	return `${role} ${nominee} did not receive enough vouches to join the circle their Nomination has expired. They can be renominated now.`;
+}
