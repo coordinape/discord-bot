a7740c454525cc7dec00a373392ac084b894d354
Karelian Pie <devkarelianpie@gmail.com>
2022-12-30 15:26:41 +0200

    feat: Add user added, removed and opted out handlers

---
+++ src/app/server/handlers/userAdded.ts
@@ -0,0 +1,69 @@
+import { ButtonBuilder, ActionRowBuilder, ButtonStyle, Role } from 'discord.js';
+import client from '../../app';
+import { COORDINAPE_BUTTON } from '../components';
+import { isTextChannel } from '../utils';
+import { Request, Response } from 'express';
+import { z } from 'zod';
+import { getUsername } from '../utils/getUsername';
+
+/**
+ * curl --request POST \
+  --url http://localhost:4000/api/epoch/user-added \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"channelId": "1057926498524332083",
+	"roleId": "1058334400540061747",
+	"discordId": "578033839910289408",
+	"address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+	"circleName": "Circumference",
+	"methodOfAddition": "Successful Vouch"
+}
+'
+ */
+
+const UserAdded = z
+	.object({
+		channelId: z.string(),
+		roleId: z.string(),
+		discordId: z.string().optional(),
+		address: z.string().optional(),
+		circleName: z.string(),
+		methodOfAddition: z.string(),
+	})
+	.partial({ discordId: true, address: true })
+	.refine(({ discordId, address }) => discordId || address, 'Either discordId or address should be filled in.');
+
+type TUserAdded = Omit<z.infer<typeof UserAdded>, 'channelId'>;
+
+export default async function handler(req: Request, res: Response) {
+	try {
+		const { channelId, ...data } = UserAdded.parse(req.body);
+		
+		const channel = await client.channels.fetch(channelId);
+		if (!channel || !isTextChannel(channel)) {
+			throw new Error('Channel not found!');
+		}
+
+		const actions: ButtonBuilder[] = ['Note', 'Link'].map((label) => (new ButtonBuilder()
+			.setCustomId(`${label.toUpperCase()}_BUTTON`)
+			.setLabel(label)
+			.setStyle(ButtonStyle.Primary)));
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents([...actions, COORDINAPE_BUTTON]);
+		
+		const guild = await client.guilds.fetch(channel.guildId);
+		const role = await guild.roles.fetch(data.roleId);
+		if (!role) {
+			throw new Error(`Role with id '${data.roleId}' not found!`);
+		}
+
+		const message = await channel.send({ content: await getContent({ role, ...data }), components: [row] });
+
+		res.status(200).send({ createdAt: message.createdTimestamp, body: req.body });
+	} catch (error) {
+		res.status(400).send(error);
+	}
+}
+
+async function getContent({ role, discordId, address, circleName, methodOfAddition }: { role: Role } & TUserAdded) {
+	return `${role}, ${getUsername({ discordId, address })} has been added to the ${circleName} circle via ${methodOfAddition}\n\nDon't forget to welcome them to the Circle with a note in the next Epoch!`;
+}
