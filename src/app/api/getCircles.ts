406bade5865d0ab1e8ba46b8074a9f4ccd142eca
Karelian Pie <devkarelianpie@gmail.com>
2023-01-14 10:07:36 +0200

    refactor: Add a check for presence profile id

--- src/app/api/getCircles.ts
+++ src/app/api/getCircles.ts
@@ -8,6 +8,11 @@ export type Circle = {
 
 export async function getCircles({ userId }: { userId: string }): Promise<Circle[]> {
 	const profileId = await getProfileId({ userId });
+
+	if (!profileId) {
+		throw new Error('profileId is required to get the circles');
+	}
+
 	const { circles } = await gqlBot('query')({
 		circles: [
 			{ where:{ users: { profile: { id: { _eq: profileId } }, role: { _eq: 1 } } } },
