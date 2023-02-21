21ce00bb4667ec33010d48ecb1168efc5e5be5aa
Karelian Pie <devkarelianpie@gmail.com>
2023-02-21 16:20:43 +0200

    fix: Fetch circle without api token

--- src/app/api/findCircle.ts
+++ src/app/api/findCircle.ts
@@ -1,17 +1,37 @@
-import { gqlApiKey } from './gqlClients';
+import { gqlApiKey, gqlBot } from './gqlClients';
 
 type Props = {
 	channelId: string;
-	apiKey: string
+	apiKey?: string
 }
 
 export async function findCircle({ channelId, apiKey }: Props) {
-	const { discord_roles_circles } = await gqlApiKey(apiKey)('query')({
-		discord_roles_circles: [
-			{ where: { discord_channel_id: { _eq: channelId } } },
-			{ circle: { id: true, name: true } },
-		],
-	});
+	let result = null;
+
+	if (!apiKey) {
+		result = await gqlBot('query')({
+			discord_roles_circles: [
+				{ where: { discord_channel_id: { _eq: channelId } } },
+				{
+					circle: { name: true },
+					circle_id: true,
+				},
+			],
+		});
+	} else {
+		result = await gqlApiKey(apiKey)('query')({
+			discord_roles_circles: [
+				{ where: { discord_channel_id: { _eq: channelId } } },
+				{
+					circle: { name: true },
+					circle_id: true,
+				},
+			],
+		});
+	}
+
+	const { discord_roles_circles } = result;
+
 
 	if (discord_roles_circles.length === 0) {
 		throw new Error(`No circle found for discord channel id ${channelId}`);
