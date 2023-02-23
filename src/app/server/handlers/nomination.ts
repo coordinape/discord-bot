e92f86498ac724e2a3f25ede3e4c199654d32944
Karelian Pie <devkarelianpie@gmail.com>
2023-02-23 21:06:01 +0200

    feat: Use safeParse and add a few logs

--- src/app/server/handlers/nomination.ts
+++ src/app/server/handlers/nomination.ts
@@ -5,6 +5,7 @@ import { COORDINAPE_BUTTON } from '../components';
 import { isTextChannel } from '../utils';
 import { Request, Response } from 'express';
 import { z } from 'zod';
+import Log from 'src/app/utils/Log';
 
 /**
  * curl --request POST \
@@ -35,31 +36,53 @@ type TNomination = Omit<z.infer<typeof Nomination>, 'channelId'>;
 
 export default async function handler(req: Request, res: Response) {
 	try {
-		const { channelId, ...data } = Nomination.parse(req.body);
+		Log.debug(`Handling nomination ${req.body}`);
+		
+		const response = Nomination.safeParse(req.body);
+
+		if (!response.success) {
+			Log.debug(`Invalid request body: ${response.error}`);
+			throw new Error(`Invalid request body: ${response.error}`);
+		}
+
+		const { channelId, ...data } = response.data;
 		
 		const channel = await client.channels.fetch(channelId);
 		if (!channel || !isTextChannel(channel)) {
-			throw new Error('Channel not found!');
+			Log.debug(`Channel id ${channelId} not found!`);
+			throw new Error(`Channel id ${channelId} not found!`);
 		}
 
+		// TODO Change for production
 		const VOUCH_BUTTON: ButtonBuilder = new ButtonBuilder()
-			.setURL(`https://app.coordinape.com/circles/${data.circleId}/members`)
+			.setURL(`https://coordinape-git-staging-coordinape.vercel.app/circles/${data.circleId}/members`)
 			.setLabel('Vouch')
 			.setStyle(ButtonStyle.Link);
 		
 		const row = new ActionRowBuilder<ButtonBuilder>().addComponents([VOUCH_BUTTON, COORDINAPE_BUTTON]);
 		
 		const guild = await client.guilds.fetch(channel.guildId);
+		if (!guild) {
+			Log.debug(`Guild id ${channel.guildId} not found! ${guild}`);
+			throw new Error(`Guild id ${channel.guildId} not found!`);
+		}
+
 		const role = await guild.roles.fetch(data.roleId);
 		if (!role) {
+			Log.debug(`Role with id '${data.roleId}' not found! ${role}`);
 			throw new Error(`Role with id '${data.roleId}' not found!`);
 		}
 
 		const message = await channel.send({ content: await getContent({ role, ...data }), components: [row] });
+		if (!message) {
+			Log.debug(`Failed to send message to channel id ${channelId}! ${message}`);
+			throw new Error(`Failed to send message to channel id ${channelId}!`);
+		}
 
 		res.status(200).send({ createdAt: message.createdTimestamp, body: req.body });
 	} catch (error) {
-		res.status(400).send(JSON.stringify(error));
+		Log.debug(`Error: ${error}`);
+		res.status(400).send(error);
 	}
 }
 
