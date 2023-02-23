a846c838df7e9c5947428e8b0a4abaac1a90bcf9
Karelian Pie <devkarelianpie@gmail.com>
2023-02-23 13:39:59 +0200

    fix: Missing apostrophe in don't

--- src/app/interactions/componentInteractions/handlers/configuration/2.3_handleSkipLinkCircleButton.ts
+++ src/app/interactions/componentInteractions/handlers/configuration/2.3_handleSkipLinkCircleButton.ts
@@ -11,7 +11,7 @@ export async function handleSkipLinkCircleButton(ctx: ComponentContext) {
 		await disableAllParentComponents(ctx);
 	
 		await ctx.send({
-			content: 'Without linking the circle you will not be able to manage the circle from discord. If you want to link the circle later you can use the `/oordinape` command and click the configure button. Also, don\t forget to delete both this channel and the role associated with it.',
+			content: 'Without linking the circle you will not be able to manage the circle from discord. If you want to link the circle later you can use the `/oordinape` command and click the configure button. Also, don\'t forget to delete both this channel and the role associated with it.',
 		});
 	} catch (error) {
 		await ctx.send(`Something is wrong, please try again or contact coordinape: [handleSkipLinkCircleButton] ${error}`);
