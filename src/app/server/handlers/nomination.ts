abf09ef251205b5af681295d8f341c9dfc6ffb46
Chef Karelian <78794805+karelianpie@users.noreply.github.com>
2023-02-23 20:56:41 +0200

    feat: Add logs to the nomination handler (#79)

--- src/app/server/handlers/nomination.ts
+++ src/app/server/handlers/nomination.ts
@@ -5,15 +5,16 @@ import { COORDINAPE_BUTTON } from '../components';
 import { isTextChannel } from '../utils';
 import { Request, Response } from 'express';
 import { z } from 'zod';
+import Log from 'src/app/utils/Log';
 
 /**
  * curl --request POST \
-  --url http://localhost:4000/api/epoch/nomination \
+  --url https://coordinape-discord-bot.herokuapp.com/api/epoch/nomination \
   --header 'Content-Type: application/json' \
   --data '{
-	"channelId": "1072574945206480997",
-	"roleId": "1058334400540061747",
-	"circleId": "5",
+	"channelId": "1078378830433099928",
+	"roleId": "1078378845759082587",
+	"circleId": "2663",
 	"nominee": "John Doe",
 	"nominator": "Jane Doe",
 	"nominationReason": "great contributions",
@@ -35,11 +36,19 @@ type TNomination = Omit<z.infer<typeof Nomination>, 'channelId'>;
 
 export default async function handler(req: Request, res: Response) {
 	try {
-		const { channelId, ...data } = Nomination.parse(req.body);
+		Log.debug('Handling nomination', req.body);
+		
+		const response = Nomination.safeParse(req.body);
+
+		if (!response.success) {
+			throw new Error(`Invalid request body: ${response.error}`);
+		}
+
+		const { channelId, ...data } = response.data;
 		
 		const channel = await client.channels.fetch(channelId);
 		if (!channel || !isTextChannel(channel)) {
-			throw new Error('Channel not found!');
+			throw new Error(`Channel id ${channelId} not found!`);
 		}
 
 		const VOUCH_BUTTON: ButtonBuilder = new ButtonBuilder()
@@ -50,16 +59,23 @@ export default async function handler(req: Request, res: Response) {
 		const row = new ActionRowBuilder<ButtonBuilder>().addComponents([VOUCH_BUTTON, COORDINAPE_BUTTON]);
 		
 		const guild = await client.guilds.fetch(channel.guildId);
+		if (!guild) {
+			throw new Error(`Guild id ${channel.guildId} not found!`);
+		}
+
 		const role = await guild.roles.fetch(data.roleId);
 		if (!role) {
 			throw new Error(`Role with id '${data.roleId}' not found!`);
 		}
 
 		const message = await channel.send({ content: await getContent({ role, ...data }), components: [row] });
+		if (!message) {
+			throw new Error(`Failed to send message to channel id ${channelId}!`);
+		}
 
 		res.status(200).send({ createdAt: message.createdTimestamp, body: req.body });
 	} catch (error) {
-		res.status(400).send(JSON.stringify(error));
+		res.status(400).send(error);
 	}
 }
 
