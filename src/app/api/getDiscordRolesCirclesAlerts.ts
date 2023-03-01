97259c2f553d2a13e2d3f105fedc0a7fe581a305
Chef Karelian <78794805+karelianpie@users.noreply.github.com>
2023-03-01 19:24:02 +0200

    fix: getDiscordRolesCirclesAlerts (#99)

--- src/app/api/getDiscordRolesCirclesAlerts.ts
+++ src/app/api/getDiscordRolesCirclesAlerts.ts
@@ -8,7 +8,11 @@ export async function getDiscordRolesCirclesAlerts({ channelId }: {channelId: st
 		],
 	});
 
-	return discord_roles_circles[0] as {
+	if (!discord_roles_circles || discord_roles_circles.length === 0) {
+		throw new Error('Something is wrong, please contact coordinape');
+	}
+
+	return discord_roles_circles?.[0] as {
 		discord_channel_id: string;
 		alerts: Record<string, boolean>;
 	};
