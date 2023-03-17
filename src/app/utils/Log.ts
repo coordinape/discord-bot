e03cbea81618f81663ed07ce3c200c3201f055f4
Chef Karelian <78794805+karelianpie@users.noreply.github.com>
2023-03-17 09:44:50 +0200

    refactor: Remove logger dna (#110)

--- src/app/utils/Log.ts
+++ src/app/utils/Log.ts
@@ -1,24 +1,15 @@
 /* eslint-disable no-console */
-import logdna, { Logger, LogOptions } from '@logdna/logger';
 import apiKeys from '../service/constants/apiKeys';
 import { CommandContext } from 'slash-create';
 import * as Sentry from '@sentry/node';
-
-let logger: Logger;
 
 try {
 	if (!apiKeys.logDNAToken) {
 		throw new Error('logDNAToken is missing');
 	}
 
-	logger = logdna.createLogger(apiKeys.logDNAToken, {
-		app: apiKeys.logDNAAppName,
-		level: apiKeys.logDNADefault,
-	});
-	if (process.env.NODE_ENV != 'production' || !logger.info) {
+	if (process.env.NODE_ENV != 'production') {
 		console.debug('Logger initialized!');
-	} else {
-		logger.log('Logger initialized!');
 	}
 } catch (e) {
 	console.log('Please setup LogDNA token.', e);
@@ -27,98 +18,56 @@ try {
 
 const Log = {
 	
-	info(statement: string | any, options?: Omit<LogOptions, 'level'>): void {
-		if (process.env.NODE_ENV != 'production' || !logger.info) {
-			console.log(statement);
-		} else {
-			logger.info(statement, options);
-		}
-	},
-	
-	warn(statement: string | any, options?: Omit<LogOptions, 'level'>): void {
-		if (process.env.NODE_ENV != 'production' || !logger.warn) {
-			console.log(statement);
-		} else {
-			logger.warn(statement, options);
-		}
-	},
-	
-	debug(statement: string | any, options?: Omit<LogOptions, 'level'>): void {
-		if (process.env.NODE_ENV != 'production' || !logger.debug) {
-			console.debug(statement);
-		} else {
-			logger.debug(statement, options);
-		}
-	},
-	
-	error(statement: string | any, options?: Omit<LogOptions, 'level'>): void {
-		if (process.env.NODE_ENV != 'production' || !logger.error) {
-			console.error(statement);
-		} else {
-			logger.error(statement, options);
-		}
-	},
-	
-	fatal(statement: string | any, options?: Omit<LogOptions, 'level'>): void {
-		if (process.env.NODE_ENV != 'production' || !logger.fatal) {
-			console.error(statement);
-		} else {
-			logger.fatal(statement, options);
-		}
-	},
-	
-	trace(statement: string | any, options?: Omit<LogOptions, 'level'>): void {
-		if (process.env.NODE_ENV != 'production' || !logger.trace) {
-			console.log(statement);
-		} else {
-			logger.trace(statement, options);
-		}
-	},
-	
-	log(statement: string | any, options?: Omit<LogOptions, 'level'>): void {
+	info(statement: string | any): void {
 		if (process.env.NODE_ENV != 'production') {
 			console.log(statement);
 		}
-		logger.log(statement, options);
 	},
 	
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	addMetaProperty(key: string, value: any): void {
-		logger.addMetaProperty(key, value);
+	warn(statement: string | any): void {
+		if (process.env.NODE_ENV != 'production') {
+			console.log(statement);
+		}
 	},
 	
-	removeMetaProperty(key: string): void {
-		logger.removeMetaProperty(key);
+	debug(statement: string | any): void {
+		if (process.env.NODE_ENV != 'production') {
+			console.debug(statement);
+		}
 	},
 	
-	flush(): void {
-		logger.flush();
+	error(statement: string | any): void {
+		if (process.env.NODE_ENV != 'production') {
+			console.error(statement);
+		}
+	},
+	
+	fatal(statement: string | any): void {
+		if (process.env.NODE_ENV != 'production') {
+			console.error(statement);
+		}
+	},
+	
+	trace(statement: string | any): void {
+		if (process.env.NODE_ENV != 'production') {
+			console.log(statement);
+		}
+	},
+	
+	log(statement: string | any): void {
+		if (process.env.NODE_ENV != 'production') {
+			console.log(statement);
+		}
 	},
 };
 
 export const LogUtils = {
 	logCommandStart(ctx: CommandContext): void {
-		Log.info(`/${ctx.commandName} ran ${ctx.user.username}#${ctx.user.discriminator}`, {
-			indexMeta: true,
-			meta: {
-				guildId: ctx.guildID,
-				userTag: `${ctx.user.username}#${ctx.user.discriminator}`,
-				userId: ctx.user.id,
-				params: ctx.options,
-			},
-		});
+		Log.info(`/${ctx.commandName} ran ${ctx.user.username}#${ctx.user.discriminator}`);
 	},
 	
 	logCommandEnd(ctx: CommandContext): void {
-		Log.info(`/${ctx.commandName} ended ${ctx.user.username}#${ctx.user.discriminator}`, {
-			indexMeta: true,
-			meta: {
-				guildId: ctx.guildID,
-				userTag: `${ctx.user.username}#${ctx.user.discriminator}`,
-				userId: ctx.user.id,
-				params: ctx.options,
-			},
-		});
+		Log.info(`/${ctx.commandName} ended ${ctx.user.username}#${ctx.user.discriminator}`);
 	},
 	
 	logError(message: string, error: Error | any, guildId?: string): void {
@@ -126,15 +75,6 @@ export const LogUtils = {
 			if (error != null && error instanceof Error) {
 				Sentry.captureException(error, {
 					tags: {
-						guildId: guildId,
-					},
-				});
-				Log.error(message, {
-					indexMeta: true,
-					meta: {
-						name: error?.name,
-						message: error?.message,
-						stack: error?.stack,
 						guildId: guildId,
 					},
 				});
